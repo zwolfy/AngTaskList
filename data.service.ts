aee69ed45b00e592923a10fed65e7a2d6a71df44
Ammy <14087614+zwolfy@users.noreply.github.com>
2021-07-27 15:38:26 -0400

    Add files via upload

---
+++ data.service.ts
@@ -0,0 +1,18 @@
+
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs/internal/BehaviorSubject';
+
+@Injectable()
+
+export class DataService {
+
+  private goals = new BehaviorSubject<any>(['']);
+  goal = this.goals.asObservable();
+
+  constructor() { }
+
+  changeGoal(goal : any) {
+    this.goals.next(goal);
+  }
+
+}
